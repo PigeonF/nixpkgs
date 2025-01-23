f8d8756429245ac869397f28e66ecfb04051e373
Jonas Fierlings <fnoegip@gmail.com>
2025-01-23 16:22:30 +0100

    committed: 1.1.2 -> 1.1.5

--- pkgs/by-name/co/committed/package.nix
+++ pkgs/by-name/co/committed/package.nix
@@ -7,9 +7,11 @@
   testers,
   nix-update-script,
   committed,
+  git,
+  zlib,
 }:
 let
-  version = "1.1.2";
+  version = "1.1.5";
 in
 rustPlatform.buildRustPackage {
   pname = "committed";
@@ -19,11 +21,26 @@ rustPlatform.buildRustPackage {
     owner = "crate-ci";
     repo = "committed";
     tag = "v${version}";
-    hash = "sha256-dBNtzKqaaqJrKMNwamUY0DO9VCVqDyf45lT82nOn8UM=";
+    hash = "sha256-puv64/btSEkxGNhGGkh2A08gI+EIHWjC+s+QQDKj/ZQ=";
   };
-  cargoHash = "sha256-F+6pTxgr/I3DcDGZsfDjLe0+5wj9Mu7nqghyOzSWlvc=";
+  cargoHash = "sha256-jJbJsVc4nYPfxH1KG/UeJhKLCYNIcr21MnNP6QoX9VY=";
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
+    darwin.apple_sdk.frameworks.Security
+    zlib
+  ];
+
+  nativeCheckInputs = [
+    git
+  ];
+
+  # Ensure libgit2 can read user.name and user.email for `git_signature_default`.
+  # https://github.com/crate-ci/committed/blob/v1.1.5/crates/committed/tests/cmd.rs#L126
+  preCheck = ''
+    export HOME=export HOME=$(mktemp -d)
+    git config --global user.name nobody
+    git config --global user.email no@where
+  '';
 
   passthru = {
     tests.version = testers.testVersion { package = committed; };
